be9d1a1ce93205af749be7beb6e0cdb8fdcd9de7
Martin Korsgaard <martin@korsgaard.name>
2023-05-30 21:26:32 +0200

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace TrayApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Tray.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrayApp
+{
+    internal class Tray : ApplicationContext
+    {
+        private NotifyIcon _trayIcon;
+
+        public Tray()
+        {
+            _trayIcon = new NotifyIcon()
+            {
+                Icon = new Icon("icon.ico"),
+                ContextMenuStrip = new ContextMenuStrip
+                {
+                    Items =
+                    {
+                        new ToolStripMenuItem("Test", null, new EventHandler(OnTest), "TEST"),
+                        new ToolStripSeparator(),
+                        new ToolStripMenuItem("Exit", null, new EventHandler(OnExit), "EXIT")
+            }
+                },
+                Visible = true
+            };
+        }
+
+        public void OnTest(object? sender, EventArgs e)
+        {
+            // do stuff here
+            MessageBox.Show("TEST was clicked!");
+        }
+
+        public void OnExit(object? sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
